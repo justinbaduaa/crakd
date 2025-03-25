9b906341c883d3dd5b6c21c7240c1cb58ba8b12f
drebs justin badua <justinbadua@drebss-MacBook-Pro.local>
2025-03-25 18:13:46 -0400

    updating auth form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from 'react'
 
 const Authlayout = ({ children }: {children: ReactNode}) => {
   return (
-    <div>layout</div>
+    <div className="auth-layout">{children}</div>
   )
 }
 
